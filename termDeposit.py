cd904659c88f1bea1eac3de8cc05254c4ff3cfbf
macsiwase <github.impairers@simplelogin.co>
2026-03-04 17:28:25 +0100

    added data leakage variable comment, univariate and bivariate sections

--- termDeposit.py
+++ termDeposit.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.20.2"
+__generated_with = "0.20.4"
 app = marimo.App()
 
 
@@ -54,6 +54,8 @@ def _(mo):
     - Data types are as expected
     - Min balance is -8019 which seems quite high. But it could make sense if the spending habits are horrible. Also this is average over the year. The spending could be much larger in specific months and does not really represent the entire year.
     - The std for balance seems very large but it does make sense given that the min is -8019, while the median is 407. The max is also 102,127, which contributes to the large std. Since median < mean, it is right skewed.
+
+    **Note**: `duration` is the length of the phone call in seconds. This is a target leakage variable because we only know the value after the call has ended. At prediction time (before calling a customer), this feature won't exist yet. It should be excluded from the final model or carefully evaluated (test with and without) otherwise it will likely inflate accuracy artificially.
     """)
     return
 
@@ -73,7 +75,7 @@ def _(mo):
     - Education at ~ 3.8% is manageable and
     - Contact at ~ 32% is unsettling. It's a third of our data.
 
-    It is quite strange that this marketing campaign has unknown values. For now let's check the class balance.
+    It is quite strange that this marketing campaign has unknown contact values. For now let's check the class balance.
     """)
     return
 
@@ -98,6 +100,24 @@ def _(df, pl):
 def _(mo):
     mo.md(r"""
     92% of no's. Huge imbalance in the dataset. Is this a normal figure for marketing efforts? Also this variable states whether the client has subscribed to a term deposit; do we know if it is due to being ignored (not picking up calls) or because they said no (if duration > 0 but no contracts)?
+
+    Let's continue and look at some plots.
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ### Univariate Analysis
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    histograms for age, balance, duration, campaign. No need day.
     """)
     return
 
@@ -108,29 +128,37 @@ def _():
 
 
 @app.cell
-def _(df, px):
+def _(mo):
+    mo.md(r"""
+    ### Bivariate Analysis
+    """)
+    return
+
+
+@app.cell
+def _(df, mo, px):
     fig = px.scatter_matrix(
         df.collect(),
         dimensions=["age", "balance", "day", "duration", "campaign"],
         color="y",
     )
-    return (fig,)
 
-
-@app.cell
-def _(fig):
-    fig.show()
+    mo.ui.plotly(fig)
     return
 
 
 @app.cell
 def _(mo):
     mo.md(r"""
-    - Check what these unknown variables are. Do we need to remove them/change the values into something more meaningful?
-    - Check class balance
-    - Plot distribution, check skewness etc. Histograms, KDE etc.
-    - Outliers?
-    - Target vs features: box plots, kde, heatmaps etc.
+    This is the initial pairwise scatter matrix of numeric features. We can understand more in our univariate and bivariate sections later.
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+ 
     """)
     return
 
